04a0aa5c96ccf271cb4f33877895ed21d6b42f0a
Hambalieu <hambalieujallow@gmail.com>
2022-09-11 10:30:37 -0700

    created a guessing game

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# guessing_game
+# Guessing Game
+
+### - In this app the user attempts to guess the secret number      
+<br />
+
+###  - The user gets three tries to try to guess the secret number.
---
+++ app.py
@@ -0,0 +1,12 @@
+secret_number = 5
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    guess = int(input("Guess the secret number: "))
+    guess_count += 1
+    if guess == secret_number:
+      print("You won!")
+else:
+  print("You lost sorry!")      
+
